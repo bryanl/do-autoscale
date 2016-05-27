dbcc3ba45e05d0876f725087152f1adf54683151
bryanl <bryanliles@gmail.com>
2016-05-27 09:38:44 -0400

    use vendored migrate bin

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -3,4 +3,4 @@
 
 DB_URL=postgres://${AUTOSCALE_DB_USER}:${AUTOSCALE_DB_PASSWORD}@${AUTOSCALE_DB_HOST}:${AUTOSCALE_DB_PORT}/${AUTOSCALE_DB_NAME}?sslmode=disable
 
-migrate -url $DB_URL -path src/autoscale/db/migrations $@
+bin/migrate -url $DB_URL -path src/autoscale/db/migrations $@
