d4c57c160539f56ad395136a352c1731372597e9
bryanl <bryanliles@gmail.com>
2016-06-07 11:09:58 -0400

    tweak status page

--- src/autoscale/dashboard/app/controllers/groups/show/status.js
+++ src/autoscale/dashboard/app/controllers/groups/show/status.js
@@ -2,138 +2,138 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  afterModel: function(model) {
-    model.group.reload();
-  },
+    afterModel: function(model) {
+        model.group.reload();
+    },
 
-  resourceStatus: Ember.computed('model', function () {
-    const model = this.get('model').group;
+    resourceStatus: Ember.computed('model', function() {
+        const model = this.get('model').group;
 
-    var maxY = 0;
-    var histories = model.get('scaleHistory').serialize();
-    for (let history of histories) {
-      if (history.total > maxY) {
-        maxY = history.total;
-      }
+        var maxY = 0;
+        var histories = model.get('scaleHistory').serialize();
+        for (let history of histories) {
+            if (history.total > maxY) {
+                maxY = history.total;
+            }
+        }
+
+        var now = new Date();
+        var lines = [
+            { value: this.relativeDate(now, 270) },
+            { value: this.relativeDate(now, 180) },
+            { value: this.relativeDate(now, 90) },
+            { value: this.relativeDate(now, 0) }];
+
+        var obj = {
+            data: {
+                type: 'line',
+                json: model.get('scaleHistory').serialize(),
+                keys: {
+                    x: 'createdAt',
+                    xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
+                    value: ['total'],
+                },
+                x: 'x',
+                xFormat: '%Y-%m-%dT%H:%M:%S.%LZ'
+            },
+
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d %I:%M',
+                        count: 4
+                    }
+                },
+                y: {
+                    tick: {
+                        format: function(d) { return d.toFixed(2).replace(/\.?0*$/, ''); }
+                    }
+                }
+            },
+
+            grid: {
+                x: {
+                    lines: lines,
+                },
+                y: {
+                    show: true
+                }
+            },
+
+            point: {
+                show: false,
+            }
+        };
+
+        return obj;
+    }),
+
+
+    metricStatus: Ember.computed('model.group.timeseriesValues', function() {
+        var values = this.get('model.group.timeseriesValues');
+
+        var json = [];
+        values.forEach((x) => {
+            json.push(x.serialize());
+        });
+
+        var now = new Date();
+        var lines = [
+            { value: this.relativeDate(now, 270) },
+            { value: this.relativeDate(now, 180) },
+            { value: this.relativeDate(now, 90) },
+            { value: this.relativeDate(now, 0) }];
+
+        var obj = {
+            data: {
+                type: 'line',
+                json: json,
+                keys: {
+                    x: 'timestamp',
+                    xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
+                    value: ['value'],
+                },
+                x: 'x',
+                xFormat: '%Y-%m-%dT%H:%M:%S.%LZ'
+            },
+
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d %I:%M',
+                        count: 4
+                    }
+                },
+                y: {
+                    tick: {
+                        count: 4,
+                        format: function(d) { return d.toFixed(2).replace(/\.?0*$/, ''); }
+
+                    }
+                }
+            },
+
+            grid: {
+                x: {
+                    lines: lines,
+                },
+                y: {
+                    show: true
+                }
+            },
+
+            point: {
+                show: false,
+            }
+
+        };
+
+        return obj;
+    }),
+
+    relativeDate: function(now, relativeTime) {
+        return (new Date(now - relativeTime * 60 * 1000)).toISOString();
     }
-
-    var yValues = [];
-    maxY = maxY + (maxY * 0.10);
-
-    for (var i = 0; i < 4; i++) {
-      yValues[i] = Math.ceil(maxY) / 3 * i;
-    }
-
-    var now = new Date();
-    var lines = [
-      { value: this.relativeDate(now, 270) },
-      { value: this.relativeDate(now, 180) },
-      { value: this.relativeDate(now, 90) },
-      { value: this.relativeDate(now, 0) }];
-
-    var obj = {
-      data: {
-        type: 'line',
-        json: model.get('scaleHistory').serialize(),
-        keys: {
-          x: 'createdAt',
-          xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
-          value: ['total'],
-        },
-        x: 'x',
-        xFormat: '%Y-%m-%dT%H:%M:%S.%LZ'
-      },
-
-      axis: {
-        x: {
-          type: 'timeseries',
-          tick: {
-            format: '%Y-%m-%d %I:%M',
-            count: 4
-          }
-        },
-        y: {
-          tick: {
-            values: yValues,
-            count: yValues.length
-          }
-        }
-      },
-
-      grid: {
-        x: {
-          lines: lines,
-        },
-        y: {
-          show: true
-        }
-      },
-
-      point: {
-        show: false,
-      }
-    };
-
-    return obj;
-  }),
-
-
-  metricStatus: Ember.computed('model.group.timeseriesValues', function () {
-    var values = this.get('model.group.timeseriesValues');
-
-    var json = [];
-    values.forEach((x) => {
-      json.push(x.serialize());
-    });
-
-    var obj = {
-      data: {
-        type: 'line',
-        json: json,
-        keys: {
-          x: 'timestamp',
-          xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
-          value: ['value'],
-        },
-        x: 'x',
-        xFormat: '%Y-%m-%dT%H:%M:%S.%LZ'
-      },
-
-      axis: {
-        x: {
-          type: 'timeseries',
-          tick: {
-            format: '%Y-%m-%d %I:%M',
-            count: 4
-          }
-        },
-        y: {
-          tick: {
-            // values: yValues,
-            count: 4,
-          }
-        }
-      },
-
-      grid: {
-        x: {
-          // lines: lines,
-        },
-        y: {
-          show: true
-        }
-      },
-
-      point: {
-        show: false,
-      }
-
-    };
-
-    return obj;
-  }),
-
-  relativeDate: function (now, relativeTime) {
-    return (new Date(now - relativeTime * 60 * 1000)).toISOString();
-  }
 });
